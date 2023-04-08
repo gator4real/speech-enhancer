74b237d8fe1293c70ccf61ac944a4e5cf3e2c16e
mike <gatorforreal@gmail.com>
2023-04-07 20:40:16 -0500

    enhances speech

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ export const enhance = (text) => {
     "d*********": "dicksucker",
     "d**********": "dicksucking",
     "b****": "bitch",
+    "w****": "whore",
   };
 
   const words = text.split(" ");
