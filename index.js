f3ecba36c2c556cffb536f9ac339596b86f2cd7b
mike <gatorforreal@gmail.com>
2023-04-07 19:05:59 -0500

    enhances speech

---
+++ index.js
@@ -0,0 +1,33 @@
+export const enhance = (text) => {
+  const profanityList = {
+    "f***": "fuck",
+    "f****": "fucks",
+    "f*****": "fucker",
+    "f******": "fucking",
+    "s***": "shit",
+    "b******": "bitches",
+    "b*******": "bullshit",
+    "s*******": "shitting",
+    "a*******": "asshole",
+    "c***": "cunt",
+    "m*********": "motherfuck",
+    "m************": "motherfucking",
+    "m***********": "motherfucker",
+    "c***": "cock",
+    "c*********": "cocksucker",
+    "c**********": "cocksucking",
+    "d*********": "dicksucker",
+    "d**********": "dicksucking",
+    "b****": "bitch",
+  };
+
+  const words = text.split(" ");
+  const filteredWords = words.map((word) => {
+    if (word in profanityList) {
+      return profanityList[word];
+    } else {
+      return word;
+    }
+  });
+  return filteredWords.join(" ");
+};
